7965b0aa21ff5fb0f604685f4f262a24ddd6bea1
Jaremy Creechley <creechley@gmail.com>
2016-12-19 16:04:31 -0700

    Changing default behavior of clocks.

--- CycleTimesManager.hpp
+++ CycleTimesManager.hpp
@@ -32,12 +32,32 @@ class CycleTimesManager
       this->periods[cycle_name] = time_ms / this->cycle_period_ms;
     }
 
-    bool is_period_up_for(TimeEnum cycle_name) {
+    void set_period_cycles(TimeEnum cycle_name, uint16_t cycle_count) {
+      this->periods[cycle_name] = cycle_count;
+    }
+
+    uint16_t get_period(TimeEnum cycle_name) {
+      return this->periods[cycle_name];
+    }
+
+    void tick(TimeEnum cycle_name) {
+      this->last_cycle_clock[cycle_name] = tock();
+    }
+
+    void start(TimeEnum cycle_name) {
+      this->tick(cycle_name);
+    }
+
+    void reset(TimeEnum cycle_name) {
+      this->last_cycle_clock[cycle_name] = 0;
+    }
+
+    bool timer_up(TimeEnum cycle_name) {
       uint16_t clocks_names_last_cycle = this->last_cycle_clock[cycle_name];
       uint16_t clocks_names_period = this->periods[cycle_name];
 
-      if (clocks_names_period == 0)
-        return true; // Always return true if cycle_names period is unset
+      if (clocks_names_period == 0 || clocks_names_last_cycle == 0)
+        return false; // Always return true if cycle_names period is unset
 
       if (clocks_names_last_cycle + clocks_names_period < tock())
       {
