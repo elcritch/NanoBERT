91137cf8552559403e7d4bcee19fb9f7a0fb4c1a
Jaremy Creechley <creechley@gmail.com>
2016-12-29 00:31:10 -0700

    Static alloc, more

--- NanoBERT.h
+++ NanoBERT.h
@@ -37,31 +37,96 @@ struct RpcMessage
     RpcMessageTypes type;
     uint8_t module;
     uint8_t method;
-    StreamBuff data;
-
-    RpcMessage() : data() {}
-
-    RpcMessage(StreamBuff db) : data(db) {}
+    StreamBuff * data;
 };
 
-template<size_t S>
+template<size_t T>
 struct RpcMessageContainer : public RpcMessage
 {
+    StreamBuffStack<T> static_data;
 
-  RpcMessageContainer() : RpcMessage(StreamBuffStack<S>()) {}
-
+    RpcMessageContainer() {
+      this->data = &static_data;
+    }
 };
 
 
 void copyStream(StreamBuff& outstream, StreamBuff& instream);
 
-template<size_t T>
-RpcMessage rpcMessageRead(StreamBuffStack<T>& instream);
 
-template<class _ARG_WRITER, size_t T>
+template<size_t T>
+RpcMessageContainer<T> rpcMessageRead(StreamBuffStack<T>& instream)
+{
+  // Read message
+  RpcMessageContainer<T> rpc_msg;
+
+  bool res = true;
+  bool res0 = true;
+  bool res1 = true;
+  bool res2 = true;
+  bool res3 = true;
+  bool res4 = true;
+
+  uint32_t msg_arr_size;
+  res &= msgpck_read_array_size(&instream, &msg_arr_size);
+  res0 &= msgpck_read_bin(&instream, &rpc_msg.version, 1);
+  res1 &= msgpck_read_bin(&instream, (uint8_t*)&rpc_msg.type, 1);
+  res2 &= msgpck_read_bin(&instream, &rpc_msg.module, 1);
+  res3 &= msgpck_read_bin(&instream, &rpc_msg.method, 1);
+  res4 &= (rpc_msg.version == RpcMessageTypes::MAGIC_VERSION);
+
+  if (!res) {
+    // Serial.println(F("rpc msg err"));
+    rpc_msg.type = RpcMessageTypes::Error;
+    rpc_msg.method = RpcProtocolCodes::UnableToReadHeader;
+    return rpc_msg;
+  }
+
+  // Remaining portion is the data packet
+  rpc_msg.data->flush();
+  copyStream(rpc_msg.static_data, instream);
+  // rpc_msg.data = outstream;
+
+  if (!res) {
+    // Serial.println(F("msg err"));
+    rpc_msg.type = RpcMessageTypes::Error;
+    rpc_msg.method = RpcProtocolCodes::UnableToReadHeader;
+    return rpc_msg;
+  }
+
+
+  return rpc_msg;
+}
+
+
+template<size_t T>
+void rpcReplyMessageHeader(StreamBuffStack<T>& outstream, StreamBuffStack<T>& data)
+{
+  uint8_t magic_version = MAGIC_VERSION;
+  uint8_t msg_type = RpcMessageTypes::Reply;
+  msgpck_write_array_header(&outstream, 3);
+  msgpck_write_bin(&outstream, &magic_version,1);
+  msgpck_write_bin(&outstream, &msg_type,1);
+  copyStream(outstream, data);
+}
+
+template<size_t T>
+void rpcNoReplyMessageHeader(StreamBuffStack<T>& outstream)
+{
+  uint8_t magic_version = MAGIC_VERSION;
+  uint8_t msg_type = RpcMessageTypes::NoReply;
+  msgpck_write_array_header(outstream, 2);
+  msgpck_write_bin(outstream, &magic_version,1);
+  msgpck_write_bin(outstream, &msg_type,1);
+}
+
+// template<size_t T>
+// RpcMessageContainer<T> rpcMessageRead(StreamBuffStack<T>& instream);
+
+template<class _ARG_WRITER>
 void rpcMessageWrite(RpcMessage& rpc_msg, _ARG_WRITER _arg_writer)
 {
-  StreamBuff * ostream = &rpc_msg.data;
+  StreamBuff * ostream = rpc_msg.data;
 
   // Read message
   bool res = true;
